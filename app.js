4cb01e0df27b8a98e1c8459c7b3fbae5016f27ef
Sahil3537 <sahilcgpt10@gmail.com>
2025-06-24 14:48:48 +0530

    final working

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+ const routes = require('./routes/mailroutes')
+
+
+
+
+app.use(express.json())
+
+app.use('/', routes)
+
+
+
+app.listen(port,()=>{
+    console.log('server is running on port 3000');
+    
+})
+
--- controller/smtp controler.js
+++ controller/smtp controler.js
@@ -1,22 +1,48 @@
 const transporter = require('../mail config/smtp_config')
-require('dotenv').config() 
-exports.sendEmail = async(req, res)=>{
-    const {to, subject, text}= req.body
+require('dotenv').config()
+const path = require('path')
+exports.sendEmail = async (req, res) => {
+    const { to, subject, body, service } = req.body
+
+
+    const config = transporter(service)
+    
+    const image = ` <div style="max-width:600px;margin:auto;">
+      <img 
+        src="https://res.cloudinary.com/ddeonryiq/image/upload/v1750752612/pexels-rdne-7563675_lgzbb9.jpg"
+        alt="Banner"
+        style="width:100%;display:block;" />
+      <h2 style="background:#4285f4;color:white;padding:10px;text-align:center;">Welcome!</h2>
+      <p style="padding:20px;">Click below to start:</p>
+    </div>  ` ;
+
+const htmlbody = config.temlate.replace(`{{image}}`, image )
+
+    //validation 
+
+    if (!config) {
+        return res.status(400).json({ success: false, message: 'Invalid service' });
+    }
+
 
     const mailOptions = {
-       from: '"Support Team" <no-reply@yourdomain.com>',
-        to, 
+        from: config.from,
+        to,
         subject,
-         text
+        text: body,
+        html: htmlbody
+
+
+
     }
 
     try {
-        const info = await transporter.sendMail(mailOptions)
-        res.json({message:'mail sent successfully', statusbar:200, success:true})
-        
+        const info = await config.transporter.sendMail(mailOptions)
+        res.json({ message: 'mail sent successfully', statusbar: 200, success: true, data: info })
+
     } catch (error) {
         console.log('error>>>> ', error.message);
-        res.json({message:"error while sending mail", success:false, statusbar:500})
-        
+        res.json({ message: "error while sending mail", success: false, statusbar: 500 })
+
     }
 }
